e10084f08b24b7b23108556571abdb24c092b8eb
Kevin Hu <hu.kevin.w@gmail.com>
2019-02-20 07:05:53 -0500

    Add working redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/about-us">About</Link>
         </header>
+
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about-us" component={About} />
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default App
