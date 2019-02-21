10fe7f03d38b87d93cd890d54566c318aef140cf
Kevin Hu <hu.kevin.w@gmail.com>
2019-02-21 09:48:13 -0500

    Add comments for todos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,30 @@ import Plant from './pages/plant';
 
 import './App.scss';
 import Navbar from './components/Navbar';
+/*
+1) Implement a NodeJS / Express RESTful API that:
+  authentication support(JWT or Sessions)
+  Login, Logout and Sign Up
 
+  Provides CRUD support for following entities:
+    Users
+    Plants
+  Users can post comments on Plants
+
+2) Implement a React SPA that:
+  Consumes the above Node RESTful server
+  Provides authentication support
+    Login / Logout / Sign Up
+    Posting comments is only allowed for logged in users
+
+  Data Consumption
+    //Redux(or similar approach like MobX) ecosystem for handling
+    //redux - thunk or redux - saga or promises
+  Responsive Design
+    //Sass or similar pre - processor approach(css -in -js)
+  ! Refrain from using tools like Bootstrap or other component libraries 
+  ? Flexbox is an ideal approach
+*/
 class App extends Component {
   render() {
     return (
