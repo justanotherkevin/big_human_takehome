15a9577ccda98d62e6de30354ed74ce8636cac53
Kevin Hu <hu.kevin.w@gmail.com>
2019-02-21 08:36:18 -0500

    Add comments to plant model

--- backend/models/Plant.js
+++ backend/models/Plant.js
@@ -5,6 +5,18 @@ const PlantSchema = new Schema({
   name: { type: String, require: true },
   details: { type: String },
   avatar: { type: String },
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+    }
+  ]
 });
 
 module.exports = Plant = mongoose.model('plant', PlantSchema);
