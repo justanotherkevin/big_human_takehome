2aaa83a423793c4d83f8b30480989617f36aa3e6
Kevin Hu <hu.kevin.w@gmail.com>
2019-02-25 01:12:19 -0500

    Add default passport config

---
+++ backend/config/passport.js
@@ -0,0 +1,25 @@
+// follow passport-jwt set up here: https://github.com/themikenicholson/passport-jwt
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('user');
+const keys = require('../config/secrets');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_playload, done) => {
+      User.findById(jwt_playload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
